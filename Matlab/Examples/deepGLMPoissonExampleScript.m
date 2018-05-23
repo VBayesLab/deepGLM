d1f7f524f7c64ee2a05530a3508e593fad68167e
VBayesLab <nghia.nguyen@sydney.edu.au>
2018-05-23 17:08:53 +1000

    Update examples

--- Matlab/Examples/deepGLMPoissonExampleScript.m
+++ Matlab/Examples/deepGLMPoissonExampleScript.m
@@ -1,41 +1,37 @@
 % Examples demonstate how to use deepGLM function to fit data with Poisson 
 % dependent variable
 %
-%   Copyright 2018 Minh-Ngoc Tran (minh-ngoc.tran@sydney.edu.au) and Nghia
-%   Nguyen (nghia.nguyen@sydney.edu.au)
+% Copyright 2018 
+%                Nghia Nguyen (nghia.nguyen@sydney.edu.au)
+%                Minh-Ngoc Tran (minh-ngoc.tran@sydney.edu.au) 
 %
-%   https://www.xxx.com
-%
-%   Version: 1.0
-%   LAST UPDATE: April, 2018
+% https://github.com/VBayesLab/deepGLM
+% 
+% Version: 1.0
+% LAST UPDATE: May, 2018
+
+clear
+clc
 
 clear
 clc
 
 % load data
 % load('../Data/dataBikeSharingPoisson.mat')
-% load('../Data/OnlineDeepGLMPoisson.mat')
-load('../Data/abaloneDeepGLM.mat')
+load('../Data/abaloneBART.mat')
 
 
 %% Fit deepGLM model using default setting
-% By default, 20% of observations of training data are used for validation
-% set. User can change this ratio by option 'nval'
-% The default neuron net structure is [n,10,10,1] where n is the
-% number of covariates
-nn = [2,2,2];
-% deepglm1 = deepGLM(X,y,'binomial','nval',0.15,'network',nn);
-
-% User can specify validation data to deepGLM by using 'Xval' and 'yval'
-% options. 
+nn = [10,10];
 mdl = deepGLMfit(X,y,...
               'Distribution','poisson',...
               'Network',nn,...
-              'Lrate',0.001,...
-              'Seed',1000,...
-              'BatchSize',3550,...
+              'Lrate',0.005,...
+              'BatchSize',size(X,1),...
+              'MaxEpoch',1000,...
+              'Patience',50,...
               'Verbose',10,...
-              'MaxEpoch',10000);
+              'Seed',1000);
 
 %% Plot training output    
 % Plot lowerbound
@@ -65,10 +61,8 @@ disp(['PPS on test data: ',num2str(Pred2.pps)])
 disp(['Mean Square Error on test data: ',num2str(Pred2.mse)])
 
 %% Compare with GLM Poisson
-X = [X];
-y = [y];
-X_test = [ones(size(X_test,1),1) X_test];
 mdlGLM = glmfit(X,y,'poisson');
+X_test = [ones(size(X_test,1),1) X_test];
 y_pred = exp(X_test*mdlGLM);
 ppsGLM = mean(-y_test'*X_test*mdlGLM + sum(y_pred));
 mseGLM = mean((y_test-y_pred).^2);
