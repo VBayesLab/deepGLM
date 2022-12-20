cb14f9f7d0bb8d3d1102a7e53f6040a2482dd715
VBLab <38531689+VBayesLab@users.noreply.github.com>
2022-12-20 22:51:46 +1100

    testing

--- Matlab/DeepGLM/utils/sumResidualSquared.m
+++
@@ -1,9 +0,0 @@
-function S = sumResidualSquared(y,X,W_seq,beta)
-% compute the sum_residual_squared for normal-NN model
-
-nnet_output = nnFeedForward(X,W_seq,beta);
-S = sum((y-nnet_output).^2);
-end
-
-
-
